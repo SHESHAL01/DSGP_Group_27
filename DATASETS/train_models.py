0af1a6e1296c033e7364f89d5516945ca137fbcf
sujathanethmini <sujatha.20242040@iit.ac.lk>
2026-01-31 20:20:56 +0530

    fine tune Logistic Regression model

--- DATASETS/train_models.py
+++ DATASETS/train_models.py
@@ -86,3 +86,47 @@ print(classification_report(y_test, y_pred))
 
 print("\nConfusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import accuracy_score, classification_report
+
+# Use relative path to the CSV file in your PyCharm project folder
+df = pd.read_csv('./encoded_skills_dataset.csv')
+
+X = df.drop(columns=['title'])
+y = df['title']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+log_param_grid = {
+    'C': [0.01, 0.1, 1, 10],
+    'solver': ['lbfgs', 'newton-cg'],
+    'max_iter': [500, 1000, 2000]
+}
+
+log_grid = GridSearchCV(
+    LogisticRegression(),
+    log_param_grid,
+    cv=5,
+    scoring='f1_macro',
+    n_jobs=-1
+)
+
+log_grid.fit(X_train, y_train)
+best_log = log_grid.best_estimator_
+
+print("Best Logistic Regression Parameters:")
+print(log_grid.best_params_)
+
+y_pred_log = best_log.predict(X_test)
+print("Tuned Logistic Regression Accuracy:", accuracy_score(y_test, y_pred_log))
+print(classification_report(y_test, y_pred_log))
+
